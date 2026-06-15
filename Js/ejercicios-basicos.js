e7f590ac9fd972a93a80b3b2cf926c1e4566b501
brunobuendialegaz <bruno.buendia.legaz@gmail.com>
2026-06-15 13:46:04 +0200

    crear repo

---
+++ Js/ejercicios-basicos.js
@@ -0,0 +1,105 @@
+// ejercicio 1
+/*
+let miSuperheroeFavorito = "Hulk";
+
+let miNumeroFavorito = 50;
+
+let altura = 5;
+
+let longitug = 10;
+
+let suma = altura + longitug;
+
+console.log(miSuperheroeFavorito)
+
+console.log(miNumeroFavorito)
+
+console.log(altura)
+
+console.log(longitug)
+
+console.log(suma)
+
+
+
+// ejercicio 2
+
+const jedi = {nombre: "Luke Skywalker", edad: 25}
+
+let nombre = "leia";
+
+let apellido = "Organa";
+
+let edad = 20;
+
+console.log("Soy " + nombre + " " + apellido + " tengo " + edad + " años y soy una princesa de Alderaan")
+
+*/
+
+// ejercicio 2
+
+/*
+
+const sable1 = {nombre: "Shoto de Yoda", precio: 1500};
+const sable2 = {nombre: "Sable de Darth Vader", precio: 2000};
+
+
+let precioTotal = sable1.precio + sable2.precio;
+
+console.log("El precio total son " + precioTotal + "$.")
+
+*/
+
+// ejercicio 2
+
+/*
+
+let precioBaseGlobal = 10000;
+
+// Cambia el precio base en esta linea modificando la variable
+
+precioBaseGlobal = 25000;
+
+console.log(precioBaseGlobal);
+
+const nave1 = {nombre: "Ala-X", precioBase: 50000, precioFinal: 60000};
+const nave2 = {nombre: "Halcón Milenario", precioBase: 70000, precioFinal: 80000};
+
+// Tu código para actualizar el precio final de cada nave aquí...
+
+console.log(nave1.precioFinal);
+console.log(nave2.precioFinal);
+
+nave1.precioFinal = nave1.precioBase + precioBaseGlobal;
+
+nave2.precioFinal = nave2.precioBase + precioBaseGlobal;
+
+console.log(nave1.precioFinal);
+console.log(nave2.precioFinal);
+
+*/
+
+//Ejercicio 3
+
+/* 
+
+console.log(10*5);
+console.log(10/2);
+console.log(15%9);
+
+const p = 10;
+const j = 5;
+let o = p + j; 
+
+console.log(o);
+
+const c = 10;
+const m = 5;
+let i = c * m;
+
+console.log(i)
+
+*/
+
+//Ejercicio 4
+
