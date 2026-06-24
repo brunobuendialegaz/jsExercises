d1f08c0e2aed06720604a2e89af5f8556777d738
brunobuendialegaz <bruno.buendia.legaz@gmail.com>
2026-06-24 18:48:59 +0200

    ejercicio 2

--- Js/ejercicios dom/script.js
+++ Js/ejercicios dom/script.js
@@ -26,11 +26,54 @@ console.log(span3); */
 
 // Ejercicio 2
 
-const emptyDiv = document.createElement('div');
+/* const emptyDiv = document.createElement('div');
 document.body.appendChild(emptyDiv);
 
 const newDiv = document.createElement('div');
-const newP = document.createElement('p');
-newP.textContent = 'Soy un texto creado con JS';
-newDiv.appendChild(newP);
+for (let i = 0; i < 6; i++) {
+    const newP = document.createElement('p');
+    newP.textContent = 'Soy un texto creado con JS ' + (i + 1);
+    newDiv.appendChild(newP);
+}
 document.body.appendChild(newDiv);
+
+const pDinamico = document.createElement('p');
+pDinamico.textContent = 'Soy Dinamico!';
+
+newDiv.appendChild(pDinamico);
+
+const h2 = document.body.querySelector('h2');
+h2.textContent = 'Wubba Lubba dub dub';
+
+const apps = ['Facebook', 'Netflix', 'Instagram', 'Snapchat', 'Twitter'];
+
+const divUl = document.createElement('div');
+const ul = document.createElement('ul');
+divUl.appendChild(ul);
+for (const element of apps) {
+    const li = document.createElement('li');
+    li.textContent = element;
+    ul.appendChild(li);
+}
+
+document.body.appendChild(divUl);
+
+const nodesToRemove = document.querySelectorAll('.fn-remove-me');
+nodesToRemove.forEach(node => node.remove());
+
+const allDivs = document.querySelectorAll('div');
+const pInBetween = document.createElement('p');
+pInBetween.textContent = 'Voy en medio';
+
+document.body.insertBefore(pInBetween, allDivs[1]);
+
+const divsInsert = document.querySelectorAll('div.fn-insert-here');
+
+divsInsert.forEach(div => {
+    const p = document.createElement('p');
+    p.textContent = 'Voy dentro!';
+    div.appendChild(p);
+}); */
+
+// Ejercicio 3
+
