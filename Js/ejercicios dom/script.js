a5a19414c63dc4ec9bc0ff4ed7dafe1667bdb699
brunobuendialegaz <bruno.buendia.legaz@gmail.com>
2026-06-30 12:58:37 +0200

    ej dom

--- Js/ejercicios dom/script.js
+++ Js/ejercicios dom/script.js
@@ -77,3 +77,106 @@ divsInsert.forEach(div => {
 
 // Ejercicio 3
 
+/* const countries = ['Japón', 'Nicaragua', 'Suiza', 'Australia', 'Venezuela'];
+
+const ul = document.createElement('ul');
+for (const element of countries) {
+    const li = document.createElement('li');
+    li.textContent = element;
+    ul.appendChild(li);
+}
+document.body.appendChild(ul);
+
+const p = document.querySelector('.fn-remove-me');
+p.remove();
+
+const cars = ['Mazda 6', 'Ford fiesta', 'Audi A4', 'Toyota corola'];
+
+const div = document.querySelector('[data-function="printHere"]');
+
+const ul3 = document.createElement('ul');
+
+div.appendChild(ul3);
+
+for (const element of cars) {
+    const li = document.createElement('li');
+    li.textContent = element;
+    ul3.appendChild(li);
+}
+
+const countries2 = [
+    { title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=1' },
+    { title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=2' },
+    { title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=3' },
+    { title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=4' },
+    { title: 'Random title', imgUrl: 'https://picsum.photos/300/200?random=5' }
+];
+
+for (const item of countries2) {
+    const div4 = document.createElement('div');
+    const h4 = document.createElement('h4');
+    h4.textContent = item.title;
+    const img = document.createElement('img');
+    img.src = item.imgUrl;
+    div4.appendChild(h4);
+    div4.appendChild(img);
+    document.body.appendChild(div4);
+}
+
+const btn = document.createElement('button');
+btn.textContent = 'borrar';
+btn.addEventListener('click', () => {
+    const divs = document.querySelectorAll('div');
+    const divsH4 = Array.from(divs).filter(div => div.querySelector('h4'));
+    divsH4[divsH4.length - 1].remove();
+});
+document.body.appendChild(btn);
+
+
+const divs = document.querySelectorAll('div');
+const divsH4 = Array.from(divs).filter(div => div.querySelector('h4'));
+
+for (const element of divsH4) {
+    const btn = document.createElement('button');
+    btn.textContent = 'boton div';
+    btn.addEventListener('click', () => {
+        element.remove();
+    });
+    element.appendChild(btn);
+} */
+
+// Ejercicio 4
+
+const btn = document.createElement("button");
+btn.textContent = "boton";
+btn.id = "btnToClick";
+btn.addEventListener("click", (event) => console.log(event.type));
+document.body.appendChild(btn);
+
+const inputFocus = document.querySelector(".focus");
+inputFocus.addEventListener("focus", (event) => {
+  const focus = event.target.value;
+  console.log(focus);
+});
+
+const inputValue = document.querySelector(".value");
+inputValue.addEventListener("input", (event) => {
+  console.log(event.target.value);
+});
+
+const albums = [
+  "De Mysteriis Dom Sathanas",
+  "Reign of Blood",
+  "Ride the Lightning",
+  "Painkiller",
+  "Iron Fist",
+];
+
+const ul = document.createElement("ul");
+document.body.appendChild(ul);
+
+for (const element of albums) {
+  const li = document.createElement("li");
+  li.textContent = element;
+  ul.appendChild(li);
+}
---
+++ prueba-vite/src/modules.js
@@ -0,0 +1,4 @@
+export const person = {
+  name: "Stephen",
+  surname: "Strange",
+};
